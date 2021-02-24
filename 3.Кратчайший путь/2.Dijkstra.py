cbd7f9a7c886a96a2a6892533382c4e6b681e31b
jugosoft <sardimka@gmail.com>
2021-02-24 11:19:50 +0400

    Negative tree values checked

--- 3.Кратчайший путь/2.Dijkstra.py
+++ 3.Кратчайший путь/2.Dijkstra.py
@@ -1,3 +1,4 @@
+#задаётся начальный граф
 graph = {}
 graph["start"] = {}
 graph["start"]["a"] = 6
@@ -12,7 +13,34 @@ graph["b"]["fin"] = 5
 
 graph["fin"] = {}
 
-print(graph['start'].keys())
+#инициализируем таблицу стоимостей
+costs = {}
+costs['a'] = 6
+costs['b'] = 2
+costs['fin'] = float('inf')
 
-def dijksstra():
-    pass
+#задаётся таблица родителей
+parents = {}
+parents['a'] = 'start'
+parents['b'] = 'start'
+parents['fin'] = None
+
+#проверенные узлы (узел не должен проверяться дважды)
+visited = []
+
+def check_nodes_positive(graph: dict):
+    for elem in graph.keys():
+        if isinstance(graph[elem], dict):
+                if not check_nodes_positive(graph=graph[elem]):
+                    return False
+        else:
+            for val in graph.values():
+                if val < 0:
+                    return False
+    return True
+
+def dijkstra(graph: dict):
+    if not check_nodes_positive(graph):
+        return
+
+dijkstra(graph)
