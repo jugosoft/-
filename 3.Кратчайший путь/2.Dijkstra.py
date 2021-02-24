566bf6d5e118b731e7e18baa1ac8ebcec5a2e78b
jugosoft <sardimka@gmail.com>
2021-02-24 10:18:31 +0400

    Dijkstra preparations

---
+++ 3.Кратчайший путь/2.Dijkstra.py
@@ -0,0 +1,18 @@
+graph = {}
+graph["start"] = {}
+graph["start"]["a"] = 6
+graph["start"]["b"] = 2
+
+graph["a"] = {}
+graph["a"]["fin"] = 1
+
+graph["b"] = {}
+graph["b"]["a"] = 3
+graph["b"]["fin"] = 5
+
+graph["fin"] = {}
+
+print(graph['start'].keys())
+
+def dijksstra():
+    pass
